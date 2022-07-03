4d3a9c318867586449e72d502d4eac80bcaa3d80
Vrushabh Khatik <71247340+vk3232@users.noreply.github.com>
2022-07-03 12:17:06 +0530

    Update and rename Pattern Printing to Star_Printing.java

---
+++ Basic programs/Star_Printing.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+public class Star_Printing
+{
+	public static void main (String [] args)
+	{
+	System.out.println("Increasing Triangle");
+	int n =6;
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		System.out.print("* "); // Space after *
+	System.out.println();       //new line after every row
+	}
+	
+	System.out.println("-----------------------------------------------");
+	System.out.println("Decreasing Triangle");
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n;j++)
+		System.out.print("* ");
+	System.out.println();
+	}
+
+	System.out.println("-----------------------------------------------");
+	System.out.println("Right Sided Triangle");
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n-1;j++)
+		System.out.print("  "); //first print decreasing triangle of space(two spaces)
+
+		for(int j=1;j<=i;j++)
+		System.out.print("* "); //print increasing triangle of *
+
+	System.out.println();
+	}
+	System.out.println("-----------------------------------------------");
+	System.out.println("Left  Sided Triangle");
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<i;j++)
+		System.out.print("  "); // first print increasing triangle of space(two spaces)
+		for(int j=i;j<=n;j++)
+		System.out.print("* "); // print decreasing triangle of *
+	System.out.println();
+	}
+
+	System.out.println("-----------------------------------------------");
+	System.out.println("Hill Pattern");
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i;j<=n-1;j++)
+		System.out.print("  "); //first print decreasing triangle of space(two spaces)
+
+		for(int j=1;j<i;j++)
+		System.out.print("* "); //print increasing triangle of *
+
+		for(int j=1;j<=i;j++)
+		System.out.print("* ");
+		System.out.println();
+	}
+
+	System.out.println("-----------------------------------------------");
+	System.out.println("Reverse Hill Pattern");
+
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<i;j++)
+		System.out.print("  "); // first print increasing triangle of space(two spaces)
+		for(int j=i;j<=n;j++)
+		System.out.print("* "); // print decreasing triangle of *
+		for(int j=i;j<n;j++)
+		System.out.print("* ");
+	System.out.println();
+	}
+
+	System.out.println("-----------------------------------------------");
+	System.out.println("Diamond Pattern");
+
+	for(int i=1;i<n;i++)
+	{
+		for(int j=i;j<=n-1;j++)
+		System.out.print("  "); //first print decreasing triangle of space(two spaces)
+
+		for(int j=1;j<i;j++)
+		System.out.print("* "); //print increasing triangle of *
+
+		for(int j=1;j<=i;j++)
+		System.out.print("* ");
+		System.out.println();
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<i;j++)
+		System.out.print("  "); // first print increasing triangle of space(two spaces)
+		for(int j=i;j<=n;j++)
+		System.out.print("* "); // print decreasing triangle of *
+		for(int j=i;j<n;j++)
+		System.out.print("* ");
+	System.out.println();
+	}
+
+
+	}
+}
